c6430ec9ba8c1e3b690c202fb8e30737a755f14d
Majeed Akinlusi <makinlusi@gmail.com>
2023-06-22 09:27:13 -0500

    sorted photos by race

---
+++ src/main/java/com/mak/racinator/models/RaceSorter.java
@@ -0,0 +1,55 @@
+package com.mak.racinator.models;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+public class RaceSorter {
+
+    public static void main(String[] args) {
+        String sourceDir = "src/main/resources/static/UTKFaces/data/images";
+        String destinationDir = "src/main/resources/static/UTKFaces/data/sorted_races";
+
+        File sourceDirectory = new File(sourceDir);
+        File[] files = sourceDirectory.listFiles();
+
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    try {
+                        String fileName = file.getName();
+                        int race = extractRaceFromFileName(fileName);
+                        if (race >= 0 && race <= 4) {  // Adjusted to match race values 0-4
+                            String destinationPath = destinationDir + "/race_" + race + "/" + fileName;
+                            createDirectoryIfNotExists(destinationDir + "/race_" + race);
+                            Files.copy(file.toPath(), Path.of(destinationPath), StandardCopyOption.REPLACE_EXISTING);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    private static void createDirectoryIfNotExists(String directoryPath) {
+        File directory = new File(directoryPath);
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+    }
+
+    private static int extractRaceFromFileName(String fileName) {
+        String[] parts = fileName.split("_");
+        if (parts.length >= 4) {
+            try {
+                return Integer.parseInt(parts[2]);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+        return -1;
+    }
+}
