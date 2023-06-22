d3afe7f1ea69bc8f4f77a81366d7f128547fbc1a
Majeed Akinlusi <makinlusi@gmail.com>
2023-06-22 07:41:59 -0500

    first commit

---
+++ src/main/java/com/mak/racinator/RacinatorApplication.java
@@ -0,0 +1,13 @@
+package com.mak.racinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacinatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacinatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mak/racinator/RacinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mak.racinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacinatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
